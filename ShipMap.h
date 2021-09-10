325d0b8f83eda76792f7a164f6d325b58cbdb5ca
IanStrasma <79732323+IanStrasma@users.noreply.github.com>
2021-09-10 12:30:06 -0500

    Add isGameOver() function
    Added isGameOver() function with an int that tracks lives, incrementing up when a ship is added and incrementing down when a ship is hit.
    Also added documentation for each function in this class.

--- ShipMap.h
+++ ShipMap.h
@@ -13,17 +13,52 @@
 class ShipMap
 {
 	public:
+	/** @pre None
+	*   @post The arrays are initialized and lives is set to 0
+	*/
 	ShipMap();
+
+	/** @pre None
+	*   @post None
+	*/
 	~ShipMap();
+
+	/** @pre Coordinates for the specific column and row
+	*   @post None
+	*   @return Returns true if the there is both a ship and an attempt in corrsponding spots (ship has been hit)
+	*/
 	bool isHit(int row, int col);
+
+	/** @pre None
+	*   @post Prints out the map of ships during the player's phase
+	*/
 	void printPlayerPhase();
+
+	/** @pre None
+	*   @post Prints out the map of ships during the enemy's phase
+	*/
 	void printEnemyPhase();
+
+	/** @pre Coordinates for the specific column and row
+	*   @post Adds a ship to the ship array
+	*/
 	void addShip(int row, int col);
+
+	/** @pre Coordinates for the specific column and row
+	*   @post Adds an attempt to the attempt array
+	*/
 	void addAttempt(int row, int col);
+
+	/** @pre None
+	*   @post None
+	*   @return Returns true if player has 0 lives remaining
+	*/
+	bool isGameOver();
 	
 	private:
 	bool ships[9][10];
 	bool attempts[9][10];
+	int lives;
 }
 
 #endif
