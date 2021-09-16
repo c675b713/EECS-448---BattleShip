9480c9a58e4ce396ed995dd441edb2f80557ba82
Zachary Andrews <zanzilla@ku.edu>
2021-09-16 10:42:04 -0500

    Added while loops for constructor and throw for returnMap

--- SetUp.cpp
+++ SetUp.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include "SetUp.h"
 #include "ShipMap.h"//dunno if i need this
+#include <stdexcept>
 
 SetUp::SetUp()
 {
@@ -17,43 +18,42 @@ SetUp::SetUp()
   int col2 = 0;
   int ships = 0;
   std::cout<<"Welcome to Battleship!\n";
-  for(int i = 1; i<2; i++)
+  for(int i = 1; i<2; i++)//for num of players
   {
     std::cout<<"Player "i", make your board! How many ships would you like to make?\n";
     std::cin>>ships;
-    for(int j = 0; j<ships; j++)
-    {//i need to make while for making correct row, column coordinates
-      std::cout<<"Enter the coordinates of your 1x"j+1" ship.\nRow: ";
-      if(i == 1)
+    for(int j = 0; j<ships; j++)//for num of ships
+    {
+      std::cout<<"Enter the coordinates of your 1x"j+1" ship. ";
+      do
       {
-        std::cin>>row1;
-      }
-      else
-      {
-        std::cin>>row2;
-      }
-      if(row1>9 || row1<1 || row2>9 || row2<1)
-      {
-        //need to make while loops for conditions
-      }
-      std::cout<<"Column: ";
-      if(i == 1)
-      {
-        std::cin>>col1;
-        if(col1>9 || col1<1)
+        std::cout<<"Make sure your ship is within 9x10 bounds of the board.\nRow: "
+        if(i == 1)//player condition for row
         {
-          //while loop needed instead of lone if
+          std::cin>>row1;
         }
+        else
+        {
+          std::cin>>row2;
+        }
+      }while(row1>9 || row1<1 || row2>9 || row2<1);
+      if(i == 1)//player condition for col
+      {
+        do
+        {
+          std::cin>>col1;
+          std::cout<<"Make sure your ship is within 9x10 bounds of the board.\nColumn: "
+        }while(col1>9 || col2<1);
         player1_grid.addShip(row1, col1, j+1);//assuming there will be a parameter for length of ship
       }
       else
       {
         do
         {
-          std::cout<<"Please type in column within 9x10 bounds of board: ";
+          std::cout<<"Make sure your ship is within 9x10 bounds of the board.\nColumn: ";
           std::cin>>col2;
-          player2_grid.addShip(row2, col2, j+1);
         }while(col2>9 || col2<1);
+        player2_grid.addShip(row2, col2, j+1);
       }
     }
   }
@@ -69,8 +69,8 @@ ShipMap SetUp::returnMap(int player)
   {
     return(player2_grid);
   }
-  else
+  else//if we use bool for player we don't need this
   {
-    //throw exception
+    throw std::runtime_error("Given player int out of range\n");
   }
 }
